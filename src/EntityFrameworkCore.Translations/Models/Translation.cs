dbeae5ab562977e488d59d4a1959e0c4afb7f2b0
Adriano Escalante <AdrianoEscalante@grupoma.eu>
2019-12-26 19:32:32 +0000

    • Added Translation class to allow adding/updating translations

---
+++ src/EntityFrameworkCore.Translations/Models/Translation.cs
@@ -0,0 +1,15 @@
+﻿namespace AdrianoAE.EntityFrameworkCore.Translations.Models
+{
+    public class Translation<TEntity>
+        where TEntity : class
+    {
+        internal readonly TEntity Entity;
+        internal readonly object[] LanguageKey;
+
+        public Translation(TEntity translatedEntity, params object[] languageKey)
+        {
+            Entity = translatedEntity;
+            LanguageKey = languageKey;
+        }
+    }
+}
