fb3e7ab800be84b6b9b66c6c0769ce32ad450269
Adriano Escalante <AdrianoEscalante@grupoma.eu>
2019-12-26 10:33:48 +0000

    • Added support for updates

--- src/EntityFrameworkCore.Translations/Extensions/DbContextExtensions.cs
+++ src/EntityFrameworkCore.Translations/Extensions/DbContextExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using AdrianoAE.EntityFrameworkCore.Translations.Helpers;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,8 +11,15 @@ namespace AdrianoAE.EntityFrameworkCore.Translations.Extensions
     {
         public static async Task<int> SaveChangesWithTranslations([NotNull] this DbContext context, params object[] _languageKey)
         {
+            int parameterPosition = 0;
+
             var addedEntries = context.ChangeTracker.Entries()
                 .Where(entry => entry.State == EntityState.Added
+                    && TranslationConfiguration.TranslationEntities.ContainsKey(entry.Entity.GetType().FullName))
+                .ToList();
+
+            var modifiedEntries = context.ChangeTracker.Entries()
+                .Where(entry => entry.State == EntityState.Modified
                     && TranslationConfiguration.TranslationEntities.ContainsKey(entry.Entity.GetType().FullName))
                 .ToList();
 
@@ -33,7 +38,7 @@ namespace AdrianoAE.EntityFrameworkCore.Translations.Extensions
                     translationEntity.Type.GetProperty(property.Name)?.SetValue(translation, property.GetValue(entry.Entity), null);
                 }
 
-                int parameterPosition = 0;
+                parameterPosition = 0;
                 foreach (var property in translationEntity.KeysFromLanguageEntity)
                 {
                     context.Entry(translation).Property(property.Name).CurrentValue = _languageKey[parameterPosition];
@@ -46,6 +51,53 @@ namespace AdrianoAE.EntityFrameworkCore.Translations.Extensions
                 }
 
                 context.Entry(translation).State = EntityState.Added;
+            }
+
+            foreach (var entry in modifiedEntries)
+            {
+                var translationEntity = TranslationConfiguration.TranslationEntities[entry.Entity.GetType().FullName];
+
+                PersistenceHelpers.ValidateLanguageKeys(translationEntity.KeysFromLanguageEntity, _languageKey);
+
+                var translatedEntry = context.ChangeTracker.Entries().Where(e =>
+                {
+                    if (e.Entity.GetType() != translationEntity.Type)
+                    {
+                        return false;
+                    }
+
+                    parameterPosition = 0;
+                    foreach (var property in translationEntity.KeysFromLanguageEntity)
+                    {
+                        if (!e.Property(property.Name).CurrentValue.Equals(_languageKey[parameterPosition]))
+                        {
+                            return false;
+                        }
+                    }
+
+                    foreach (var property in translationEntity.KeysFromSourceEntity)
+                    {
+                        if (!e.Property(property.Value).CurrentValue.Equals(entry.Property(property.Key).CurrentValue))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }).SingleOrDefault();
+
+                if (translatedEntry == null)
+                {
+                    throw new ArgumentNullException($"Could not find the translated entry for the entry {entry.Entity.GetType().Name}");
+                }
+
+                foreach (var property in entry.Entity.GetType().GetProperties())
+                {
+                    if (translationEntity.Type.GetProperty(property.Name) != null)
+                    {
+                        context.Entry(translatedEntry.Entity).Property(property.Name).CurrentValue = property.GetValue(entry.Entity);
+                    }
+                }
             }
 
             return result += await context.SaveChangesAsync();
